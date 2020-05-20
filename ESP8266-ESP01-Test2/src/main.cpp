a85e6dc0e1ead3b963e25ff160c567f16f4d3487
Patrick Le <patrick@Patato-PC.localdomain>
2020-05-20 13:30:32 +1000

    Updated attempt at ESP modification (Not working)

--- ESP8266-ESP01-Test2/src/main.cpp
+++ ESP8266-ESP01-Test2/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
+// Setup Software Serial for ESP8266
+
+SoftwareSerial ESPSoftwareSerial(2, 3); // Rx and Tx pins
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  ESPSoftwareSerial.begin(115200);
+  Serial.println("Setup Initialised");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Have Arduino listen for Serial communication between the ESP and the PC
+  if (ESPSoftwareSerial.available()) {
+    Serial.write(ESPSoftwareSerial.read());
+  }
+
+  // Have data read via serial monitor to be written to device
+  if (Serial.available()) {
+    ESPSoftwareSerial.write(Serial.read());
+  }
 }
